46098dc24a5a710269c9a889e49b5e25260c8e72
pakapalimt <pakapalimt@hotmail.com>
2015-07-22 14:29:41 +1200

    any not message

---
+++ prototypeIITests/prototypeIITests.m
@@ -0,0 +1,34 @@
+//
+//  prototypeIITests.m
+//  prototypeIITests
+//
+//  Created by Pakapan on 22/04/15.
+//  Copyright (c) 2015 Pakapan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface prototypeIITests : XCTestCase
+
+@end
+
+@implementation prototypeIITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
